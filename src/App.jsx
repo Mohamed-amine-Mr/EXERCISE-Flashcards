6ec6500248460017ef17178f7ca7b1386fa934f0
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-04-17 07:28:02 +0100

    adding stater files

--- src/App.jsx
+++ src/App.jsx
@@ -41,25 +41,6 @@ const questions = [
     answer: "Controlled element",
   },
 ];
-
 function FlashCards() {
-  const [selectedId, setSelectedId] = useState(null);
-
-  const handleClick = (id) => {
-    // toggle the selected state of the card
-    selectedId === id ? setSelectedId(null) : setSelectedId(id);
-  };
-  return (
-    <div className="flashcards">
-      {questions.map((q) => (
-        <div
-          className={q.id === selectedId ? "card selected" : "card"}
-          key={q.id}
-          onClick={() => handleClick(q.id)}
-        >
-          {q.id === selectedId ? q.answer : q.question}
-        </div>
-      ))}
-    </div>
-  );
+  return <div>TODO</div>;
 }
